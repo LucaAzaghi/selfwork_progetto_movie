640eb5cd595eb680a7408584625a9478ab6c1158
luca azaghi <luca.azaghi1402@libero.it>
2026-07-30 11:26:03 +0200

    collegamento database

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:progetto_movie/viewmodels/movie_view_model.dart';
 import 'package:progetto_movie/views/home_view.dart';
 import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart'; // 1. Importa Supabase
 
-void main() {
+void main() async {
+  // 2. Rendi il main async
+  WidgetsFlutterBinding.ensureInitialized(); // 3. Obbligatorio per i binding
+
+  // 4. Inizializza Supabase con i dati del tuo progetto
+  await Supabase.initialize(
+    url: 'https://ozujvgbvzrwspnexvhra.supabase.co',
+    anonKey: 'sb_publishable_MQ812UdDQrd6FuM0FP866Q_suwAWbvl',
+  );
+
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => MovieViewModel())],
@@ -15,13 +25,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Progetto Movie',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
       home: HomeView(),
     );
   }
